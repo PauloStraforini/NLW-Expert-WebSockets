e126b5ee1ac7b283d8b0e0953327ed1a892b726e
PauloStraforini <paulostraforini@gmail.com>
2025-09-17 21:36:03 -0300

    initial commit

---
+++ prisma/migrations/20250918000403_create_poll/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
