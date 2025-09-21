bae5c59825ca47fe7e9767755fa8e4e03b9db687
PauloStraforini <paulostraforini@gmail.com>
2025-09-21 15:38:04 -0300

    refactor(polls): update server and vote-on-poll logic

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,20 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-poll.js'
 import { getPoll } from './routes/get-poll.js'
 import { voteOnPoll } from './routes/vote-on-poll.js'
 
 const app = fastify()
 
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+  parseOptions: {},
+})
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
-
-const prisma = new PrismaClient()
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
