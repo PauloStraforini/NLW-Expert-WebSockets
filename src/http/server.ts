4fad97ee31d9e88994a1afa7b5db9ac1c0bef916
PauloStraforini <paulostraforini@gmail.com>
2025-09-24 22:31:21 -0300

    feat(polls): emit websocket event on new vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-poll.js'
 import { getPoll } from './routes/get-poll.js'
 import { voteOnPoll } from './routes/vote-on-poll.js'
+import fastifyWebsocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-results.js'
 
 const app = fastify()
 
@@ -12,9 +14,13 @@ app.register(cookie, {
   parseOptions: {},
 })
 
+app.register(fastifyWebsocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
